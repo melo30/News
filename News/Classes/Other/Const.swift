34be3689afe0697318bc4e69911a17d40358d867
陈诚 <281083409@qq.com>
2020-07-20 20:28:38 +0800

    增加16进制颜色函数

--- News/Classes/Other/Const.swift
+++ News/Classes/Other/Const.swift
@@ -13,9 +13,44 @@ let screenWidth = UIScreen.main.bounds.width
 /// 屏幕的高度
 let screenHeight = UIScreen.main.bounds.height
 
-//自定义颜色
+//自定义颜色(RGB方式)
 func RGBColor(r:CGFloat, g:CGFloat, b:CGFloat) -> UIColor {
     return UIColor.init(red: r/255.0, green: g/255.0, blue: b/255.0, alpha: 1)
+}
+
+//自定义颜色(16进制方式)
+func colorWithHexString(hexString:String)->UIColor{
+    var cstr = hexString.trimmingCharacters(in:  CharacterSet.whitespacesAndNewlines).uppercased() as NSString;
+    if(cstr.length < 6){
+        return UIColor.clear;
+    }
+    if(cstr.hasPrefix("0X") || cstr.hasPrefix("0x")){
+        cstr = cstr.substring(from: 2) as NSString
+    }
+    if(cstr.hasPrefix("#")){
+        cstr = cstr.substring(from: 1) as NSString
+    }
+    if(cstr.length != 6){
+        return UIColor.clear;
+    }
+    var range = NSRange.init()
+    range.location = 0
+    range.length = 2
+    //r
+    let rStr = cstr.substring(with: range);
+    //g
+    range.location = 2;
+    let gStr = cstr.substring(with: range)
+    //b
+    range.location = 4;
+    let bStr = cstr.substring(with: range)
+    var r :UInt32 = 0x0;
+    var g :UInt32 = 0x0;
+    var b :UInt32 = 0x0;
+    Scanner.init(string: rStr).scanHexInt32(&r);
+    Scanner.init(string: gStr).scanHexInt32(&g);
+    Scanner.init(string: bStr).scanHexInt32(&b);
+    return UIColor.init(red: CGFloat(r)/255.0, green: CGFloat(g)/255.0, blue: CGFloat(b)/255.0, alpha: 1);
 }
 
 //KeyWindow
